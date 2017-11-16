19295e207c16e244a3befcdfdde9da507773f7c3
ms_rc_sklep <ms_rc_sklep@outlook.com>
2017-11-16 22:32:12 +0100

    Wyswietlanie Customers oraz Movies ( twarde dane)

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModel;
@@ -10,24 +7,39 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movie
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer {Name="Customer 1" },
-                new Customer {Name="Customer 2" },
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
 
             var viewModel = new RandomMovieViewModel
             {
-                Customers = customers,
-                Movie = movie
+                Movie = movie,
+                Customers = customers
             };
 
             return View(viewModel);
         }
-        
     }
 }
